b5f7509c84c4bfaff2b49d88cecfbacbc5d4ca79
Michael C <71363695+EuphosiouX@users.noreply.github.com>
2021-06-22 23:10:22 +0700

    Delete projectFinal directory

--- projectFinal/src/projectfinal/interfaces/CheckTextField.java
+++
@@ -1,8 +0,0 @@
-// interfaces package
-package projectfinal.interfaces;
-
-// ChectTextField interface
-public interface CheckTextField {
-    // Declare methods
-    public boolean textFieldIsEmpty();
-}
